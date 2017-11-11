5111778de3dc045f8aee938a408f6e688293d709
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2017-11-12 01:28:07 +0500

    added express

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
  * @copyright: farrukh mamatkhalilov 2017
  * @repo: github.com/webstyle/env_manager
  */
+
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+app.get('/', (req, res) => res.render('index', {title: 'ENV manager', message: 'Hello there! Coming soon...'}));
+
+app.listen(port, console.log(`Server is run on port ${port}`));
