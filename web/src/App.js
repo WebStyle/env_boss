52f0c7efc082ba3a9b33d5dc04111566a93e0225
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2017-11-12 02:23:00 +0500

    refactor

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import './animate.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">ENV Manager</h1>
+        <header className="App-header animated fadeInDown">
+          <img src={logo} className="App-logo animated infinite pulse" alt="logo"/>
+          <h1 className="App-title">Manage your environments like a boss.</h1>
         </header>
-        <p className="App-intro">
-          Manage your environments easier.
+        <p className="App-intro animated fadeInUp">
+          Content
         </p>
       </div>
     );
