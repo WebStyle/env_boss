5c3324e8947374963e1b15f78b8717fc89266a15
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2017-11-12 08:31:36 +0500

    added settings and routes

--- index.js
+++ index.js
@@ -8,13 +8,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const routes = require('./routes/index');
+
 const app = express();
-const port = 5000;
+const port = require('./settings.json').port;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(routes);
 
 app.listen(port, console.log(`Server is run on port ${port}`));
