51c32b4b15c05fa59ce63d0ae80c2e6f95133b01
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2017-11-12 08:21:26 +0500

    added body parser

--- index.js
+++ index.js
@@ -6,8 +6,15 @@
  */
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
+
 
 app.listen(port, console.log(`Server is run on port ${port}`));
